e56db56d682f86afbab03da4dff182ccd87cb1a3
Sandy1501 <81281765+Sandy1501@users.noreply.github.com>
2021-11-17 14:09:09 -0600

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
